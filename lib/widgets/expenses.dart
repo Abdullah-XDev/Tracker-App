69d7659f1f6caa46dc14c0f46ddce0e55cf4cf8d
dev <dev@test.com>
2024-05-25 03:31:50 +0300

    update

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -18,44 +18,79 @@ class _ExpensesState extends State<Expenses> {
         amount: 39.9,
         date: DateTime.now(),
         category: Category.work),
-        Expense(
+    Expense(
         title: 'Networks course',
         amount: 49.9,
         date: DateTime.now(),
         category: Category.leisure),
-         Expense(
-        title: 'Palestine',
-        amount: 100,
-        date: DateTime.now(),
-        category: Category.travel),
-         Expense(
-        title: 'Rewsturant',
-        amount: 35.5,
-        date: DateTime.now(),
-        category: Category.food),
   ];
 
-  void _openAddExpenseOverlay(){
-    showModalBottomSheet<void>(
+  void _openAddExpenseOverlay() {
+    showModalBottomSheet(
+      isScrollControlled: true,
       context: context,
-      builder: (ctx)=> const NewExpense(),);
+      builder: (ctx) => NewExpense(
+        onAddExpense: _addExpense,
+      ),
+    );
+  }
+
+  void _addExpense(Expense expense) {
+    setState(() {
+      _registredExpenses.add(expense);
+    });
+  }
+
+  void _removeExpense(Expense expense) {
+    final expenseIndex = _registredExpenses.indexOf(expense);
+    setState(() {
+      _registredExpenses.remove(expense);
+    });
+    ScaffoldMessenger.of(context).clearSnackBars();
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        duration: const Duration(seconds: 3),
+        content: const Text('Expense Deleted'),
+        action: SnackBarAction(
+            label: 'Undo',
+            onPressed: () {
+              setState(() {
+                _registredExpenses.insert(expenseIndex, expense);
+              });
+            }),
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
+    Widget mainContent = const Center(
+      child: Text('No Expenses found. Start adding some?!'),
+    );
+
+    if (_registredExpenses.isNotEmpty) {
+      mainContent = ExpensesList(
+        expenses: _registredExpenses,
+        onRemoveExpense: _removeExpense,
+      );
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Expense Tracker'),
         actions: [
           IconButton(
             onPressed: _openAddExpenseOverlay,
-            icon: const Icon(Icons.add),)
+            icon: const Icon(Icons.add),
+          )
         ],
       ),
       body: Column(
-        children:[
+        children: [
           const Text('The chart'),
-          Expanded(child: ExpensesList(expenses:_registredExpenses)),
+          Expanded(
+            child: mainContent,
+          ),
         ],
       ),
     );
--- lib/widgets/expenses_list/expenses_list.dart
+++ lib/widgets/expenses_list/expenses_list.dart
@@ -3,16 +3,22 @@ import 'package:tracker/models/expense.dart';
 import 'package:tracker/widgets/expenses_list/expenses_item.dart';
 
 class ExpensesList extends StatelessWidget {
-  const ExpensesList({super.key, required this.expenses});
+  const ExpensesList(
+      {super.key, required this.expenses, required this.onRemoveExpense});
 
   final List<Expense> expenses;
+  final void Function(Expense expense) onRemoveExpense;
 
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
-      itemCount: expenses.length,
-      itemBuilder: (ctx, index) => ExpenseItem(expense: expenses[index]),
-    );
+        itemCount: expenses.length,
+        itemBuilder: (ctx, index) => Dismissible(
+              key: ValueKey(expenses[index]),
+              onDismissed: (direction) {
+                onRemoveExpense(expenses[index]);
+              },
+              child: ExpenseItem(expense: expenses[index]),
+            ));
   }
 }
-
